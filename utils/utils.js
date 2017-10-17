caf903db01e4e54499240ee16dce5d779b4cfeb1
loconluis <luis.locon@gmail.com>
2017-10-16 22:04:07 -0600

    Testing async code

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,22 @@
-// Sum two numbers 
+// Sum two numbers
 module.exports.add = (a, b) => a + b
+// async add function
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    let res = a + b
+    callback(res)
+  }, 1000)
+}
 // Square a number
 module.exports.square = (x) => x * x
-
+// async square function
+module.exports.asyncSquare = (x, callback) => {
+  setTimeout(() => {
+    let res = x * x
+    callback(res)
+  }, 1000)
+}
+// Set a name and lastname for object user
 module.exports.setName = (user, fullname) => {
   let names = fullname.split(' ')
   user.firstName = names[0]
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,3 +1,4 @@
+
 const expect = require('expect')
 
 const util = require('./utils')
@@ -11,9 +12,16 @@ it('Should add two numbers', () => {
   //   throw new Error(`Expected 44, but got ${res}`)
   // }
 })
+// Test for add function (but async way)
+it('should async add two numbers', (done) => {
+  util.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number')
+    done() // use done to tell mocha this is a Async function
+  })
+})
 
 // Test for square function
-it('Should square the number', ()=> {
+it('Should square the number', () => {
   const res = util.square(2)
 
   expect(res).toBe(4).toBeA('number')
@@ -21,13 +29,20 @@ it('Should square the number', ()=> {
   //   throw new Error(`Expected 4, but got ${res}`)
   // }
 })
+// Test for square function (but async way)
+it('Should async square the number', (done) => {
+  util.asyncSquare(3, (sqr) => {
+    expect(sqr).toBe(9).toBeA('number')
+    done()
+  })
+})
 
 it('should expect some values', () => {
   // expect(12).toNotBe(11);
   // expect({name: 'Luis'}).toEqual({name: 'Luis'})
   // expect([2,3,4]).toInclude(2)
   // expect([2,3,4]).toExclude(5)
-  expect ({
+  expect({
     name: 'Luis',
     age: 24,
     location: 'Guatemala'
@@ -36,10 +51,10 @@ it('should expect some values', () => {
   })
 })
 
-it('should verify first and last name are set', ()=>{
+it('should verify first and last name are set', () => {
   let user = {location: 'Guatemala', age: 24}
   let res = util.setName(user, 'Luis Locon')
-  
+
   // expect(res).toEqual(res)
   expect(res).toInclude({
     firstName: 'Luis',
