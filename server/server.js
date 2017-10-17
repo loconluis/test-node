f49dcb1e254cb015e64ab0ab2f0b6bed4899e470
loconluis <luis.locon@gmail.com>
2017-10-16 22:58:44 -0600

    Using supertest to test https request and expect to nest test for our express app

--- server/server.js
+++ server/server.js
@@ -1,11 +1,51 @@
 const express = require('express')
 
 const app = express()
+let users = [
+  {
+    name: 'Luis',
+    age: 24
+  },
+  {
+    name: 'Alfonso',
+    age: 21
+  },
+  {
+    name: 'Carlos',
+    age: 22
+  }
+]
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  })
 })
 
-app.listen(3000, () => {
-  console.log('Server is running at port 3000')
+app.get('/users', (req, res) => {
+  res.status(200).send(
+    [
+      {
+        name: 'Luis',
+        age: 24
+      },
+      {
+        name: 'Alfonso',
+        age: 21
+      },
+      {
+        name: 'Carlos',
+        age: 22
+      }
+    ]
+  )
 })
+
+if (!module.parent) { // this help us cause test run the server, to kill that run we use this
+  app.listen(3000, () => {
+    console.log('Server is running at port 3000')
+  })
+}
+
+module.exports.app = app
---
+++ server/server.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+const expect = require('expect')
+
+const app = require('./server').app
+
+it('Should return hello world response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      })
+    })
+    .end(done)
+})
+
+it('Should send an array of users', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Luis',
+        age: 24
+      })
+    })
+    .end(done)
+})
